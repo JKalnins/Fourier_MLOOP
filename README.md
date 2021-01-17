a96276c1cbd7fc1fd791f01f3ffcd9b3fe6ce7a0
JKalnins <kalnins.jacob@gmail.com>
2021-01-17 16:51:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fourier_MLOOP
+ Streamlining Module for running M-LOOP optimising Fourier Series and saving results
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="floop",
+    version="0.1",
+    description="Runs M-LOOP using Fourier Series and save outputs",
+    url="#",
+    author="Jacob",
+    install_requires=["numpy", "m-loop"],
+    author_email="",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
